f187370e06810776d4a55c46836370ba50f55d08
nullspace000 <nullspace000@protonmail.com>
2026-04-06 17:33:15 -0600

    commiting files

---
+++ project/DESIGN.md
@@ -0,0 +1,58 @@
+# Design Document
+
+By Diego Couto
+
+Video overview: <URL HERE>
+
+## Scope
+
+In this section you should answer the following questions:
+
+* What is the purpose of your database?
+  - I'm not a soccer fan myself, but I have this Idea for an app: To represent small football leagues. It would allow users to keep track of tournaments, create their own team, publish match location and date and more.
+
+* Which people, places, things, etc. are you including in the scope of your database?
+  - Leagues, Tournaments, matches, teams, players, users
+
+* Which people, places, things, etc. are *outside* the scope of your database?
+  - Venues, referees and player specific performance data.
+
+## Functional Requirements
+
+In this section you should answer the following questions:
+
+* What should a user be able to do with your database?
+  - Create their user account.
+  - Register themselves as a player or a manager.
+  - Create a team.
+  - Add other players to their team.
+  -
+* What's beyond the scope of what a user should be able to do with your database?
+
+## Representation
+
+### Entities
+
+In this section you should answer the following questions:
+
+* Which entities will you choose to represent in your database?
+* What attributes will those entities have?
+* Why did you choose the types you did?
+* Why did you choose the constraints you did?
+
+### Relationships
+
+In this section you should include your entity relationship diagram and describe the relationships between the entities in your database.
+
+## Optimizations
+
+In this section you should answer the following questions:
+
+* Which optimizations (e.g., indexes, views) did you create? Why?
+
+## Limitations
+
+In this section you should answer the following questions:
+
+* What are the limitations of your design?
+* What might your database not be able to represent very well?
---
+++ project/schema.sql
@@ -0,0 +1,207 @@
+-- In this SQL file, write (and comment!) the schema of your database, including the CREATE TABLE, CREATE INDEX, CREATE VIEW, etc. statements that compose it
+
+-- Represents users
+CREATE TABLE "users" (
+    "id" INTEGER,
+    "email" TEXT NOT NULL UNIQUE,
+    "password" TEXT NOT NULL CHECK(LENGTH("password") >= 10),
+    "creation_timestamp" NUMERIC NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "banned" TEXT NOT NULL CHECK("banned" IN ('true', 'false')) DEFAULT 'false',
+    PRIMARY KEY("id")
+);
+
+-- Represents players
+CREATE TABLE "players" (
+    "id" INTEGER,
+    "user_id" INTEGER,
+    "first_name" TEXT NOT NULL UNIQUE,
+    "last_name" TEXT NOT NULL UNIQUE,
+    "shirt_number" INTEGER NOT NULL,
+    "position" TEXT NOT NULL CHECK("position" IN ('Goalkeeper', 'Defender', 'Midfield', 'Forward')),
+    "birth_country" TEXT NOT NULL,
+    "birth_state" TEXT NOT NULL,
+    "birth_year" INTEGER NOT NULL,
+    "last_game" INTEGER,
+    "creation_timestamp" NUMERIC NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY("id"),
+    FOREIGN KEY("user_id") REFERENCES "users"("id")
+);
+
+-- Represents trainers
+CREATE TABLE "trainers" (
+    "id" INTEGER,
+    "user_id" INTEGER,
+    "first_name" TEXT NOT NULL,
+    "last_name" TEXT NOT NULL,
+    "birth_country" TEXT NOT NULL,
+    "birth_state" TEXT NOT NULL,
+    "birth_year" INTEGER NOT NULL,
+    "creation_timestamp" NUMERIC NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY("id"),
+    FOREIGN KEY("user_id") REFERENCES "users"("id")
+);
+
+-- Represents referees
+CREATE TABLE "referees" (
+    "id" INTEGER,
+    "user_id" INTEGER,
+    "first_name" TEXT NOT NULL,
+    "last_name" TEXT NOT NULL,
+    "birth_country" TEXT NOT NULL,
+    "birth_state" TEXT NOT NULL,
+    "birth_year" INTEGER NOT NULL,
+    "creation_timestamp" NUMERIC NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY("id"),
+    FOREIGN KEY("user_id") REFERENCES "users"("id")
+);
+
+-- Represents teams
+CREATE TABLE "teams" (
+    "id" INTEGER,
+    "name" TEXT NOT NULL,
+    "bio" TEXT NOT NULL,
+    "foundation_year" NUMERIC NOT NULL,
+    -- is_active will automatically be set to 'false' after two years of inactivity
+    "is_active" INTEGER  NOT NULL CHECK ("is_active" IN ('true', 'false')) DEFAULT 'true',
+    "primary_color" TEXT NOT NULL,
+    "secondary_color" TEXT NOT NULL,
+    "creation_timestamp" NUMERIC NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY("id")
+);
+
+-- Players and teams joining table.
+-- Because a player could be in multiple teams and teams have multiple players.
+CREATE TABLE "teams_players" (
+    "player_id" INTEGER,
+    "team_id" INTEGER,
+    -- "team_id" is first in the primary key so that the index is created for "team_id"
+    PRIMARY KEY("team_id", "player_id"),
+    FOREIGN KEY("player_id") REFERENCES "players"("id"),
+    FOREIGN KEY("team_id") REFERENCES "teams"("id")
+);
+
+-- Trainers and teams joining table.
+-- Because a trainer could be in multiple teams and teams can have multiple trainers
+CREATE TABLE "teams_trainers" (
+    "trainer_id" INTEGER,
+    "team_id" INTEGER,
+    PRIMARY KEY("team_id", "trainer_id"),
+    FOREIGN KEY("trainer_id") REFERENCES "trainer"("id"),
+    FOREIGN KEY("team_id") REFERENCES "teams"("id")
+);
+
+-- Teams and administrator accounts joining table
+CREATE TABLE "teams_admins" (
+    "user_id" INTEGER,
+    "team_id" INTEGER,
+    PRIMARY KEY("team_id", "user_id"),
+    FOREIGN KEY("user_id") REFERENCES "users"("id"),
+    FOREIGN KEY("team_id") REFERENCES "teams"("id")
+);
+
+-- Represents matches
+CREATE TABLE "matches" (
+    "id" INTEGER,
+    "tournament_id" INTEGER,
+    "location" TEXT NOT NULL,
+    "date" NUMERIC NOT NULL,
+    -- remember to set scores to 0 when match_status changes to 'Ongoing'
+    "status" TEXT NOT NULL CHECK ("status" IN ('Scheduled', 'Ongoing', 'Finished', 'Postponed')) DEFAULT 'Scheduled',
+    "team_a_id" INTEGER NOT NULL,
+    "team_b_id" INTEGER NOT NULL,
+    "score_team_a" INTEGER NOT NULL,
+    "score_team_b" INTEGER NOT NULL,
+    "creation_timestamp" NUMERIC NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY("id"),
+    FOREIGN KEY("tournament_id") REFERENCES "tournaments"("id"),
+    FOREIGN KEY("team_a_id") REFERENCES "teams"("id"),
+    FOREIGN KEY("team_b_id") REFERENCES "teams"("id")
+);
+
+-- Represents match events
+CREATE TABLE "match_events" (
+    "id" INTEGER,
+    "player_id" INTEGER,
+    "team_id" INTEGER,
+    "event_type" TEXT NOT NULL CHECK("event_type" IN ('goal', 'fault', 'yellow_card', 'red_card')),
+    "creation_timestamp" NUMERIC NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY("id"),
+    FOREIGN KEY("team_id") REFERENCES "teams"("id"),
+    FOREIGN KEY("player_id") REFERENCES "players"("id")
+);
+
+-- Represents tournaments
+CREATE TABLE "tournaments" (
+    "id" INTEGER,
+    "league_id" INTEGER,
+    "name" TEXT NOT NULL,
+    "status" TEXT NOT NULL CHECK ("status" IN ('Scheduled', 'Ongoing', 'Finished', 'Postponed')) DEFAULT 'Scheduled',
+    "season" INTEGER,
+    "format" TEXT NOT NULL CHECK ("format" IN ('Knockout', 'Round Robin', 'Groups')) DEFAULT 'Knockout',
+    "start_date" NUMERIC NOT NULL,
+    "end_date" NUMERIC NOT NULL,
+    "half_time" INTEGER NOT NULL DEFAULT 45,
+    "max_teams" INTEGER NOT NULL,
+    "entry_fee" NUMERIC,
+    "points_per_win" INTEGER DEFAULT 3,
+    "points_per_draw" INTEGER DEFAULT 1,
+    "points_per_loss" INTEGER DEFAULT 0,
+    "creation_timestamp" NUMERIC NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY("id"),
+    FOREIGN KEY("league_id") REFERENCES "leagues"("id")
+);
+
+CREATE TABLE "leagues" (
+    "id" INTEGER,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "location_general" TEXT NOT NULL,
+    "creation_timestamp" NUMERIC NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY("id")
+);
+
+-- Leagues and administrator accounts joining table
+CREATE TABLE "leagues_admins" (
+    "user_id" INTEGER,
+    "league_id" INTEGER,
+    PRIMARY KEY("league_id", "user_id"),
+    FOREIGN KEY("user_id") REFERENCES "users"("id"),
+    FOREIGN KEY("league_id") REFERENCES "leagues"("id")
+);
+
+-- Matches and referees joining table
+CREATE TABLE "matches_referees" (
+    "referee_id" INTEGER,
+    "match_id" INTEGER,
+    PRIMARY KEY("match_id", "referee_id"),
+    FOREIGN KEY("referee_id") REFERENCES "referees"("id"),
+    FOREIGN KEY("match_id") REFERENCES "matches"("id")
+);
+
+-- Tournaments and teams joining table
+CREATE TABLE "tournament_teams" (
+    "tournament_id" INTEGER,
+    "team_id" INTEGER,
+    PRIMARY KEY("tournament_id", "team_id"),
+    FOREIGN KEY("tournament_id") REFERENCES "tournaments"("id"),
+    FOREIGN KEY("team_id") REFERENCES "teams"("id")
+);
+
+/*
+drop table players;
+drop table teams;
+drop table users;
+drop table trainers;
+drop table matches;
+
+drop table referees;
+drop table teams_trainers;
+drop table teams_admins;
+drop table teams_players;
+drop table tournaments;
+drop table match_events;
+
+drop table leagues;
+drop table leagues_admins;
+drop table matches_referees;
+drop table tournament_teams;*/
