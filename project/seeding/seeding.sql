8aee07eae1be44b89dbefa91ea7e87cf582edd82
nullspace000 <nullspace000@protonmail.com>
2026-04-12 16:03:37 -0600

    feat(seeding): Integrated csv imports into seeding.sql.

--- project/seeding/seeding.sql
+++ project/seeding/seeding.sql
@@ -1,17 +1,23 @@
--- users table seeding:
--- sqlite> .import --csv seeding/users_seed.csv temp
+-- users
+CREATE TEMP TABLE temp (email TEXT, password TEXT);
+.mode csv
+.import --skip 1 seeding/users_seed.csv temp
 INSERT INTO "users" ("email", "password")
 SELECT "email", "password" FROM "temp";
 DROP TABLE "temp";
 
--- players table seeding:
--- sqlite> .import --csv seeding/players_seed.csv temp
+-- players
+CREATE TEMP TABLE temp (user_id INT, first_name TEXT, last_name TEXT, birth_country TEXT, birth_state TEXT, birth_year INT);
+.mode csv
+.import --skip 1 seeding/players_seed.csv temp
 INSERT INTO "players" ("user_id", "first_name", "last_name", "birth_country", "birth_state", "birth_year")
 SELECT "user_id", "first_name", "last_name", "birth_country", "birth_state", "birth_year" FROM "temp";
 DROP TABLE "temp";
 
--- trainers table seeding:
--- sqlite> .import --csv seeding/trainers_seed.csv temp
+-- trainers
+CREATE TEMP TABLE temp (user_id INT, first_name TEXT, last_name TEXT, birth_country TEXT, birth_state TEXT, birth_year INT);
+.mode csv
+.import --skip 1 seeding/trainers_seed.csv temp
 INSERT INTO "trainers" ("user_id", "first_name", "last_name", "birth_country", "birth_state", "birth_year")
 SELECT "user_id", "first_name", "last_name", "birth_country", "birth_state", "birth_year" FROM "temp";
 DROP TABLE "temp";
