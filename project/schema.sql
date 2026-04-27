3479fa7c7d98fd622fa0855ac94561079b441e63
nullspace000 <nullspace000@protonmail.com>
2026-04-27 11:13:12 -0600

    feat: Created matches_view. It shows team names and tournament name.

--- project/schema.sql
+++ project/schema.sql
@@ -104,13 +104,12 @@ CREATE TABLE "matches" (
     "id" INTEGER,
     "tournament_id" INTEGER,
     "team_a_id" INTEGER NOT NULL,
+    "score_team_a" INTEGER,
     "team_b_id" INTEGER NOT NULL,
+    "score_team_b" INTEGER,
     "location" TEXT NOT NULL,
     "date" NUMERIC NOT NULL,
-    -- remember to set scores to 0 when match_status changes to 'Ongoing'
     "status" TEXT NOT NULL CHECK ("status" IN ('Scheduled', 'Ongoing', 'Finished', 'Postponed')) DEFAULT 'Scheduled',
-    "score_team_a" INTEGER,
-    "score_team_b" INTEGER,
     "creation_timestamp" NUMERIC NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY("id"),
     FOREIGN KEY("tournament_id") REFERENCES "tournaments"("id"),
@@ -196,26 +195,45 @@ CREATE TABLE "tournaments_teams" (
 -- matches_referees_view
 -- Joining "teams" - "matches" - "matches_referees" - "referees"
 CREATE VIEW "matches_referees_view" AS
-SELECT "team_a"."name"        AS "team_A",
-"team_b"."name"        AS "team_B",
-"matches"."date",
-"referees"."first_name",
-"referees"."last_name"
+SELECT "team_a"."name" AS "team_A",
+    "team_b"."name" AS "team_B",
+    "matches"."date",
+    "referees"."first_name",
+    "referees"."last_name"
 FROM "matches_referees"
-JOIN "matches"  ON  "matches_referees"."match_id"   = "matches"."id"
-JOIN "teams" AS "team_a" ON "matches"."team_a_id"   = "team_a"."id"
-JOIN "teams" AS "team_b" ON "matches"."team_b_id"   = "team_b"."id"
-JOIN "referees" ON "matches_referees"."referee_id"  = "referees"."id";
+JOIN "matches"  ON  "matches_referees"."match_id" = "matches"."id"
+JOIN "teams" AS "team_a" ON "matches"."team_a_id" = "team_a"."id"
+JOIN "teams" AS "team_b" ON "matches"."team_b_id" = "team_b"."id"
+JOIN "referees" ON "matches_referees"."referee_id" = "referees"."id";
+
+-- matches
+-- Joining teams - matches - tournaments
+CREATE VIEW "matches_view" AS
+SELECT
+    "tournaments"."name",
+    "team_a"."name" AS "team_A",
+    "matches"."score_team_a",
+    "team_b"."name" AS "team_B",
+    "matches"."score_team_b",
+    "matches"."location",
+    "matches"."date",
+    "matches"."status",
+    "matches"."creation_timestamp"
+FROM "matches"
+JOIN "tournaments" ON "matches"."tournament_id" = "tournaments"."id"
+JOIN "teams" AS "team_a" ON "matches"."team_a_id" = "team_a"."id"
+JOIN "teams" AS "team_b" ON "matches"."team_b_id" = "team_b"."id";
+
 
 -- teams_players_view
 -- Joing teams - teams_players - players
 CREATE VIEW "teams_players_view" AS
 SELECT "teams"."name", 
-"shirt_number", "position",
-"players"."first_name", 
-"players"."last_name", 
-"players"."birth_country", 
-"players"."birth_year"
+    "shirt_number", "position",
+    "players"."first_name", 
+    "players"."last_name", 
+    "players"."birth_country", 
+    "players"."birth_year"
 FROM "teams_players"
 JOIN "teams" ON "teams_players"."team_id" = "teams"."id"
 JOIN "players" ON "teams_players"."player_id" = "players"."id";
@@ -224,10 +242,10 @@ JOIN "players" ON "teams_players"."player_id" = "players"."id";
 -- Joing teams - teams_trainers - trainers
 CREATE VIEW "teams_trainers_view" AS
 SELECT "teams"."name", 
-"trainers"."first_name", 
-"trainers"."last_name", 
-"trainers"."birth_country", 
-"trainers"."birth_year"
+    "trainers"."first_name", 
+    "trainers"."last_name", 
+    "trainers"."birth_country", 
+    "trainers"."birth_year"
 FROM "teams_trainers"
 JOIN "teams" ON "teams_trainers"."team_id" = "teams"."id"
 JOIN "trainers" ON "teams_trainers"."trainer_id" = "trainers"."id";
@@ -252,14 +270,14 @@ JOIN "users" ON "leagues_admins"."user_id" = "users"."id";
 -- Joining tournaments - tournaments_teams - teams
 CREATE VIEW "tournaments_teams_view" AS
 SELECT "tournaments"."name", 
-"tournaments"."status", 
-"tournaments"."season", 
-"tournaments"."format", 
-"tournaments"."start_date", 
-"tournaments"."end_date", 
-"tournaments"."max_teams",
-"teams"."name", 
-"teams"."foundation_year"
+    "tournaments"."status", 
+    "tournaments"."season", 
+    "tournaments"."format", 
+    "tournaments"."start_date", 
+    "tournaments"."end_date", 
+    "tournaments"."max_teams",
+    "teams"."name", 
+    "teams"."foundation_year"
 FROM "tournaments_teams"
 JOIN "tournaments" ON "tournaments_teams"."tournament_id" = "tournaments"."id"
 JOIN "teams" ON "tournaments_teams"."team_id" = "teams"."id";
