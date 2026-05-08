5c027cbc5b0c9ae5caa55be510a135270b5c06d4
nullspace000 <nullspace000@protonmail.com>
2026-05-08 07:41:50 -0600

    feat: Removing seeding with csv's from rebuild_database.sh.

--- project/rebuild_database.sh
+++ project/rebuild_database.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/bash
 
 rm soccer_leagues_db
-cat schema.sql seeding/seeding.sql | sqlite3 soccer_leagues_db
+cat schema.sql | sqlite3 soccer_leagues_db
+cat queries.sql | sqlite3 soccer_leagues_db
+
